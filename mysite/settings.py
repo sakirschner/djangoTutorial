dc2ce889c7b4565a33cae435a25ee18ea4597f41
Scott Kirschner <scott.kirschner@perficient.com>
2020-11-23 13:32:58 -0500

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,11 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = (BASE_DIR / 'static',)
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'blog/media'
+STATIC_ROOT = BASE_DIR / 'static'
